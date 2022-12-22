462ac6b01b3e1dca87ed5cc607d31a8d14b43252
iyanuoluwa sowande <iyanusowande@gmail.com>
2022-12-22 13:30:02 +0100

    fixed next config issue

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  reactStrictMode: true,
-  swcMinify: true,
-  env: {
-    NEXT_PUBLIC_RESUME_LINK: "1Y5ttDgKB_Q3NVjPe5lzKZxYOrP7nEX5v",
-  },
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-  },
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development',
 });
+
+const nextConfig = {
+	reactStrictMode: true,
+	images: {
+		domains: ['images.ctfassets.net', 'i.ytimg.com'],
+	},
+};
+
+module.exports = module.exports = withPWA(nextConfig);
