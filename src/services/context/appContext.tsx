deba4d210e5e77da665ce69e0e0a687ea2b314d8
iyanuoluwa sowande <iyanusowande@gmail.com>
2023-01-09 14:07:00 +0100

    initial page setup

---
+++ src/services/context/appContext.tsx
@@ -0,0 +1,65 @@
+import { createContext, ReactNode, useCallback, useContext, useState } from 'react';
+import { clearState, saveState } from 'services/storage';
+
+type IAppContext = {
+	show: boolean;
+	theme: boolean;
+	loadTheme: () => void;
+	setTheme: () => void;
+	handleOpen: () => void;
+	closeShow: () => void;
+};
+
+const defaultState = {
+	show: false,
+	theme: false,
+	loadTheme: () => void 0,
+	setTheme: () => void 0,
+	handleOpen: () => void 0,
+	closeShow: () => void 0,
+};
+const AppContext = createContext<IAppContext>(defaultState);
+
+/*
+ * PROVIDER
+ */
+export const AppProvider = (props: { children: ReactNode }) => {
+	const [show, setShow] = useState(false);
+	const [theme, setTheme] = useState(false);
+
+	const loadTheme = useCallback(async () => {
+		if (!theme) {
+			clearState();
+		} else {
+			saveState(1);
+		}
+	}, []);
+
+	return (
+		<AppContext.Provider
+			value={{
+				show,
+				theme,
+				loadTheme,
+				setTheme: () => setTheme((prevTheme) => !prevTheme),
+				closeShow: () => setShow(false),
+				handleOpen: () => setShow((show) => !show),
+			}}
+		>
+			{props.children}
+		</AppContext.Provider>
+	);
+};
+
+/*
+ * HOOK
+ */
+export const useApp = () => {
+	const context = useContext(AppContext);
+	if (!context) {
+		throw new Error("Missing AppProvider. You can't use the hook, without the provider.");
+	}
+	return context;
+};
+
+export default AppContext;
---
+++ src/services/storage/index.ts
@@ -0,0 +1,31 @@
+const Storage = {
+	get: function (k: string) {
+		try {
+			const storedItem = localStorage.getItem(k);
+			if (storedItem === null) {
+				return null;
+			}
+
+			return JSON.parse(storedItem);
+		} catch (e) {
+			return null;
+		}
+	},
+	set: function <T>(k: string, v: T) {
+		localStorage.setItem(k, JSON.stringify(v));
+	},
+	remove: function (k: string) {
+		localStorage.removeItem(k);
+	},
+};
+
+export default Storage;
+
+// State utility functions
+
+const STATE = 'SHADYCODES';
+export const loadState = () => Storage.get(STATE);
+
+export const saveState = <T>(value: T) => Storage.set(STATE, value);
+
+export const clearState = () => Storage.remove(STATE);
